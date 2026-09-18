d8fd1d417cec5e8e1aa9ac348a68e4888a63036e
Aditya Kumar <aditya.cse24@gmail.com>
2026-09-18 11:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (81.89%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(m - 1, n - 1, dp);
+    }
+    int solve(int i, int j, int[][] dp){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int down = solve(i - 1, j, dp);
+        int right = solve(i, j - 1, dp);
+
+        dp[i][j] = down + right;
+        return dp[i][j];
+    }
+}
